56ade035627076d0aa56675fd12c0754f89da462
Kajosam <113198328+Kajosam@users.noreply.github.com>
2022-09-19 09:13:20 +0100

    Add files via upload

---
+++ e-Commerce.js
@@ -0,0 +1,8 @@
+function menubar(){
+    let x = document.getElementById("navbar");
+    if ( x.style.display === "none"){
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+}
